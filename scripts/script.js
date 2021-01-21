9866f7e3294a723a4afaa9dd95ff100fb366b722
Unknown <raekwongerold@hotmail.com>
2021-01-21 19:02:16 +0100

    H2 Animation, werkt nog niet goed met scroll...

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
-// JavaScript Document
+document.getElementsByTagName("h2").addEventListener(
+    'scroll',
+    function()
+    {
+        var scrollTop = document.getElementsByTagName("h2").scrollTop;
+        var scrollHeight = document.getElementsByTagName("h2").scrollHeight;
+        var offsetHeight = document.getElementsByTagName("h2").offsetHeight;
+        var contentHeight = scrollHeight - offsetHeight;
+        if (contentHeight <= scrollTop)
+        {
+            html.addEventListener('scroll', () => {
+                logo.style.webkitAnimation = 'none';
+                text.style.webkitAnimation = 'none';
+                setTimeout(function() {
+                  logo.style.webkitAnimation = '';
+                  text.style.webkitAnimation = '';
+                }, 10);
+        }
+    },
+    false
+)
