881eac2246593ed626611f971607f89e14a925cd
Unknown <raekwongerold@hotmail.com>
2021-01-21 19:22:02 +0100

    added some javascript animations? and onclick towards agents.html

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,15 @@
-document.getElementsByTagName("h2").addEventListener(
-    'scroll',
-    function()
-    {
-        var scrollTop = document.getElementsByTagName("h2").scrollTop;
-        var scrollHeight = document.getElementsByTagName("h2").scrollHeight;
-        var offsetHeight = document.getElementsByTagName("h2").offsetHeight;
-        var contentHeight = scrollHeight - offsetHeight;
-        if (contentHeight <= scrollTop)
-        {
-            html.addEventListener('scroll', () => {
-                logo.style.webkitAnimation = 'none';
-                text.style.webkitAnimation = 'none';
-                setTimeout(function() {
-                  logo.style.webkitAnimation = '';
-                  text.style.webkitAnimation = '';
-                }, 10);
-        }
-    },
-    false
-)
+// JavaScript Document
+const html = document.querySelector('html');
+const text = document.querySelector('h2');
+
+html.addEventListener('scroll', () => {
+    text.style.webkitAnimation = 'none';
+    setTimeout(function () {
+        text.style.webkitAnimation = '';
+    }, 10);
+});
+
+
+document.getElementById("agent_Button").onclick = function () {
+    location.href = "pages/agents.html";
+}
